57e01ce5564ed83393b294fcaf3943a996cd7725
Devanshu Kumar <devanshu.kumar07@gmail.com>
2019-12-08 23:13:46 -0800

    minor changes to app.py

--- app.py
+++ app.py
@@ -15,18 +15,6 @@ def init():
     print("The type is ", type(o))
     output = json.dumps(get_main_table())
     return output
-    """
-    output = get_main_table()
-    f = []
-    for row in output:
-        result = {}
-        result["id"] = row[0]
-        result["name"] = row[1]
-        result["rank"] = row[2]
-        result["type"] = row[3]
-        f.append(result)
-    return json.dumps(f)
-    """
 
 if __name__ == '__main__':
     app.run(debug=False, threaded=True, host= '0.0.0.0')
