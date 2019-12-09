c108d3e995dfa57c78483127c68d0a3ada459a1f
Devanshu Kumar <devanshu.kumar07@gmail.com>
2019-12-08 23:12:00 -0800

    added the home rendering page from SQL server

--- app.py
+++ app.py
@@ -8,14 +8,25 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
-@app.route('/init')
-def main_query():
+@app.route('/init', methods=['GET'])
+def init():
     print("Test")
     o = get_main_table()
     print("The type is ", type(o))
     output = json.dumps(get_main_table())
-    print(output)
     return output
+    """
+    output = get_main_table()
+    f = []
+    for row in output:
+        result = {}
+        result["id"] = row[0]
+        result["name"] = row[1]
+        result["rank"] = row[2]
+        result["type"] = row[3]
+        f.append(result)
+    return json.dumps(f)
+    """
 
 if __name__ == '__main__':
-    app.run(debug=True, host= '0.0.0.0')
+    app.run(debug=False, threaded=True, host= '0.0.0.0')
--- static/main.js
+++ static/main.js
@@ -1,19 +1,20 @@
 function createGrid(data){
 	var result = "";
-
+    var d =  "<thead> <tr><th>ID</th><th>University</th><th>Rank</th><th>Type</th><th>Attendance</th><th>Gender Ratio</th><th>Rank</th><th>Average Scholarship</th></tr></thead>"
 	for (var i in data){
-		var cr = "<tr>";
+        cr = "<tr>";
 
 		for (var j in data[i]){
-            cr = cr + "<td id=\"" + i.toString()+ "-" + j.toString() + "\">"+data[i]+"</td>";
+            cr = cr + "<td id=\"" + i.toString()+ "-" + j.toString() + "\">"+data[i][j]+"</td>";
             //cr = cr + "<td id=\"" + i.toString()+ "-" + j.toString() + "\">";
 		}
 
 		cr = cr + "</tr>\n";
 		result = result + cr;
-	}
+    }
+    var final = d + result
 
-	$("#college").html(result);
+	$("#college").html(final);
 
 }
 $(document).ready(function(){
@@ -22,6 +23,4 @@ $(document).ready(function(){
         var data = JSON.parse(response);
         createGrid(data);
     });
-    $("#test").html("TESTING");
-    
 });
